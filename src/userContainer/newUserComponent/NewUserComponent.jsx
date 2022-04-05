a8d198cf09cf2e7597a29da824a4b6c51f502b67
Sam <sam.stearn@gmail.com>
2022-04-04 17:18:50 -0700

    button fix, new user box size fix

--- src/userContainer/newUserComponent/NewUserComponent.jsx
+++ src/userContainer/newUserComponent/NewUserComponent.jsx
@@ -72,9 +72,6 @@ const NewUserComponent = (props) => {
                             
                             <input onChange={handleInputChange} type="text" name="userName" id="userName" placeholder="Name"value={newUser.userName}/>
                             
-                            <span style={{background: color}}onClick={changeColor}className="greenboy">a</span>
-                           
-                            
                             <button className= "submit-button" type="submit"><svg xmlns="http://www.w3.org/2000/svg" height="24px" viewBox="0 0 24 24" width="24px" fill="#000000"><path d="M0 0h24v24H0V0z" fill="none"/><path d="M9 16.2L4.8 12l-1.4 1.4L9 19 21 7l-1.4-1.4L9 16.2z"/></svg></button>
 
                         </fieldset>
